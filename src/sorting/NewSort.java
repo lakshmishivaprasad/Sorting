91b4dba46ff1be469a3bc9c065910772b212334d
SP8102 <SP8102@CSELAB01-18.bennett.edu.in>
2019-01-30 11:58:55 +0530

    hello

--- src/sorting/NewSort.java
+++ src/sorting/NewSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class NewSort { 
     public static void main(String[] args) 
     { 
-        int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 }; 
+        int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102,104 }; 
   
         Arrays.sort(arr); 
   
